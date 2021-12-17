742ae7a48ec8c12c90f791a683255dabc818f90e
Yusuf Güngör <yusufgngor@gmail.com>
2021-12-18 02:59:07 +0300

    funcA added

---
+++ GitFlow/FutureA.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  sda
+//
+//  Created by Yusuf Güngör on 18.12.2021.
+//
+
+import Foundation
+
+
+
+func funcA() {
+    print("this function is made for FutureA branch")
+    
+}
