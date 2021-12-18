7e72ac0278d920cf7be528238227687b776e754f
Yusuf Güngör <yusufgngor@gmail.com>
2021-12-18 03:03:11 +0300

    funcB added

---
+++ GitFlow/FutureB.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  sda
+//
+//  Created by Yusuf Güngör on 18.12.2021.
+//
+
+import Foundation
+
+
+
+func futureB() {
+    print("this function added for futureB Branch")
+    
+}
