4994c6cd60444d1591886dbaca563ddd0a0dccaf
Yusuf Güngör <yusufgngor@gmail.com>
2021-12-18 03:18:46 +0300

    future c is edited for rebase in develop branch

---
+++ GitFlow/FeatureC.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  sda
+//
+//  Created by Yusuf Güngör on 18.12.2021.
+//
+
+import Foundation
+
+
+
+func funcC() {
+    print("this function is made for FutureC branch and will be rebase on develop branch" )
+    
+}
